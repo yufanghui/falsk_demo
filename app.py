897319f07ac71ff2ead3a61bd721c9328d4e1f73
yufang01 <yufang01@58.com>
2023-08-22 19:32:20 +0800

    添加静态资源；数据库

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask
+# from markupsafe import escape
+# from flask import url_for
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy  
+import os
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+
+
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+    
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(60)) #电影名称
+    year = db.Column(db.String(4))  # 电影年份
+    
+
+import click
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
