84cc1d1a8e8037d48e14b1cfc8aa0fa4bd478f28
yufang01 <yufang01@58.com>
2023-08-22 16:32:53 +0800

    添加基础的路由操作

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from markupsafe import escape
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to My Watchlist!1212'
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def hi(name):
+    return f'my name is {escape(name)}'
+
+@app.route('/test')
+def test():
+    print(url_for('hello'))
+    print(url_for('hi',name='123'))
+    print(url_for('test'))
+    print(url_for('test',baa='foo'))
+    return "test page"
