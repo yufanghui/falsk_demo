6bd95c974cd3729a29420d05836c8bf7b4ca3b01
yufang01 <yufang01@58.com>
2023-08-22 16:57:12 +0800

    添加index.html

--- hello.py
+++ hello.py
@@ -1,21 +1,26 @@
 from flask import Flask
-from markupsafe import escape
-from flask import url_for
+# from markupsafe import escape
+# from flask import url_for
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
-@app.route('/user/<name>')
-def hi(name):
-    return f'my name is {escape(name)}'
 
-@app.route('/test')
-def test():
-    print(url_for('hello'))
-    print(url_for('hi',name='123'))
-    print(url_for('test'))
-    print(url_for('test',baa='foo'))
-    return "test page"
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
