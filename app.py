4324cb8f6cfe24a0db3ac0eef736d06730a4f717
yufang01 <yufang01@58.com>
2023-08-23 16:08:03 +0800

    创建，编辑，删除操作

--- app.py
+++ app.py
@@ -1,16 +1,52 @@
-from flask import Flask
-# from markupsafe import escape
-# from flask import url_for
-from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy  
 import os
+from flask import request, url_for, redirect, flash, Flask, render_template
 
 
 app = Flask(__name__)
-@app.route('/')
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input')
+            return redirect(url_for('index'))
+        movie = Movie(title=title,year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created')
+        return redirect(url_for('index'))
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input')
+            return redirect(url_for('index'))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie=movie)
+    
+
+@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -45,8 +81,6 @@ def initdb(drop):
 
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
-
-
 
 
 
